262d195432dabb6965648e629d049a42424ff1c4
Ricardo Ribeiro <ricardo.ribeiro@stoom.com.br>
2019-10-31 11:28:00 -0300

    functional version, need visual improvements

--- moduloD/HourExc/script.js
+++ moduloD/HourExc/script.js
@@ -1,40 +1,45 @@
 
 function loader () {
-    var msg = window.document.getElementById("msg");
-    var secondMsg = window.document.getElementById("second-msg");  
-    var img = window.document.getElementById("img");
-    var data = new Date()
-    var hoursInput = window.document.getElementById("hourInput").value;
-    var minutesInput = window.document.getElementById("minuteInput").value;
-
+    var msg = window.document.getElementById("msg"),
+        secondMsg = window.document.getElementById("second-msg"),
+        img = window.document.getElementById("img"),
+        data = new Date(),
+        hoursInput = window.document.getElementById("hourInput"),
+        minutesInput = window.document.getElementById("minuteInput");
 
     function setRealTime() {
-        hoursInput = data.getHours();
-        minutesInput = data.getMinutes();
-    }
-    if  (hoursInput === "" && minutesInput === "") {
-        setRealTime();
-    }
-    if ((hoursInput > 23 || hoursInput < 0) || (minutesInput > 60 || minutesInput < 0)) {
-        alert("Please, insert a valid number for hour and minutes");
-         window.document.getElementById("hourInput").value = '';
-         window.document.getElementById("minuteInput").value = '';
-        setRealTime();
-        
+        hoursInput.value = data.getHours();
+        minutesInput.value = data.getMinutes();
     }
 
-    msg.innerHTML = `It's ${hoursInput}h:${minutesInput}m.`;    
+    function clearField() {
+        window.document.getElementById("hourInput").value = '';
+        window.document.getElementById("minuteInput").value = '';
+    }
+
+    if  (hoursInput.value === "" && minutesInput.value === "") {
+        setRealTime();
+        clearField();
+    }
+
+    if ((hoursInput.value > 23 || hoursInput.value < 0) || (minutesInput.value > 60 || minutesInput.value < 0)) {
+        alert("Please, insert a valid number for hour and minutes");
+        clearField();
+        setRealTime();
+    }
+
+    msg.innerHTML = `It's ${hoursInput.value || data.getHours()}h:${minutesInput.value || data.getMinutes()}m.`;    
     
 
-    if (hoursInput >= 6 && hoursInput < 12) {
+    if (hoursInput.value >= 6 && hoursInput.value < 12) {
         img.src =`morning.png`
         document.body.style.background = "#bdf1f9";
         secondMsg.innerHTML = `If you're seeing my project I wish you a good morning`
-    }else if (hoursInput >= 12 && hoursInput < 18) {
+    } else if (hoursInput.value >= 12 && hoursInput.value < 18) {
         img.src =`afternoon.png`
         document.body.style.background = "#FFAB68";
         secondMsg.innerHTML = `If you're seeing my project I wish you a good afternoon`
-    }else {
+    } else {
         img.src =`night.png`
         document.body.style.background = "#777777";
         secondMsg.innerHTML = `If you're seeing my project I wish you a good night`
